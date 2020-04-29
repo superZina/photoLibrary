17f715406103c3e9d62722fd37910582c4058ad0
이진하 <dkwlsfk2323@gmail.com>
2020-04-29 23:42:37 +0900

    collectionViewController을 이용한 photo 앨범 틀 작성

---
+++ photoLibrary/CollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CollectionViewCell.swift
+//  photoLibrary
+//
+//  Created by 이진하 on 2020/04/29.
+//  Copyright © 2020 이진하. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var photo: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ photoLibrary/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  photoLibrary
+//
+//  Created by 이진하 on 2020/04/29.
+//  Copyright © 2020 이진하. All rights reserved.
+//
+
+import UIKit
+import Photos
+
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 15
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = photoView.dequeueReusableCell(withReuseIdentifier: "photoCell", for: indexPath) as? CollectionViewCell else {return CollectionViewCell()}
+        cell.photo.image = UIImage(named: "농담곰")
+        return cell
+    }
+    
+
+    @IBOutlet weak var photoView: UICollectionView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let photoCellNib = UINib(nibName: "CollectionViewCell", bundle: nil)
+        self.photoView.register(photoCellNib, forCellWithReuseIdentifier: "photoCell")
+        self.photoView.delegate = self
+        self.photoView.dataSource = self
+        self.photoView.reloadData()
+    }
+
+
+}
+
